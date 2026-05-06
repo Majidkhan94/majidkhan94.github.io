307abff6b395d329ce7695a6a4b4bf9b1bba9ff5
Majidkhan94 <majidkhan.dev94@gmail.com>
2026-05-06 22:36:57 +0500

    Updated MobileVersion

--- src/Features/Paragraph.jsx
+++ src/Features/Paragraph.jsx
@@ -7,7 +7,7 @@ export const Paragraph = ({ text, className }) => {
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true, amount: 0.5 }}
       transition={{ duration: 1 }}
-      className={`${className} text-ParaColor font-Paragraph text-sm md:text-base tracking-wider px-5`}>
+      className={`${className} text-ParaColor font-Paragraph text-sm md:text-base tracking-wider px-5 md:px-0`}>
       {text}
     </motion.p>
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,13 +8,13 @@ import { Seperator } from '../Features/Seperator.jsx';
 export const Home = ()=>{
   return(<>
 <section className='flex flex-col-reverse md:flex-row w-full h-fit md:h-screen pl-0 md:pl-30 '>
-  <div className='flex flex-1 flex-col gap-6 justify-center text-center'>
+  <div className='flex flex-1 flex-col gap-6 justify-center text-center md:text-left'>
     <span> <Heading text={"Full Stack Developer"}/> </span>
     <span className='w-full md:w-140'>
       <Paragraph text={"Building high-quality web applications using ASP.NET Core and MERN stack. Focused on clean code and seamless user experiences that solve real-world problems."}/>
     </span>
     <span>
-    <Button to={"/Projects"} text={"Projects"}/>
+    <Button to={"/Projects"} text={"View Projects"}/>
     <Button to={"/Contactus"} text={"Contact Us"}/>
     </span>
   </div>
