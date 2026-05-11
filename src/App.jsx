735241d7afcc4fd44ef797dd75b55d5e71127f25
Majidkhan94 <majidkhan.dev94@gmail.com>
2026-05-11 14:17:38 +0500

    Solve issues in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,33 @@ import { SplashScreen } from "./Components/SplashScreen.jsx";
 import { AnimatePresence, motion } from "framer-motion";
 
 export const App = () => {
+  
+  const [loading, setLoading] = useState(() => {
+    const currentHash = window.location.hash;
+    return currentHash === "" || currentHash === "#/" || currentHash === "#";
+  });
 
-  const isHomePage = window.location.pathname === "/";
-  const [loading, setLoading] = useState(isHomePage);
-
-useEffect(() => {
-    if (isHomePage) {
+  useEffect(() => {
+    if (loading) {
       const timer = setTimeout(() => {
         setLoading(false);
       }, 2000);
       return () => clearTimeout(timer);
     }
-  }, [isHomePage]);
+  }, [loading]);
 
-  return (<>
+  return (
     <main className="h-screen relative">
       <AnimatePresence mode="wait">
-        {loading 
-        ? ( <SplashScreen key="loader" /> )
-        : (
+        {loading ? (
+          <SplashScreen key="loader" />
+        ) : (
           <motion.div
-            key="content" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} >
+            key="content"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
             <HashRouter>
               <Routing />
             </HashRouter>
@@ -33,4 +39,5 @@ useEffect(() => {
         )}
       </AnimatePresence>
     </main>
-  </>)}
+  );
+};
